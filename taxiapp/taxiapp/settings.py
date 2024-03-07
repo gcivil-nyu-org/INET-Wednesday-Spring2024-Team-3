b4c75017503392028f1e2c8455b4bd9a294c28c6
devmcdonald <dpm360@nyu.edu>
2024-03-06 21:44:11 -0500

    modified:   taxiapp/taxiapp/settings.py

--- taxiapp/taxiapp/settings.py
+++ taxiapp/taxiapp/settings.py
@@ -10,8 +10,8 @@ env = environ.Env()
 environ.Env.read_env(env_file="secrets.env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
