ec99ff5f5c2eac3f287328551dcad247cfc191d0
devmcdonald <dpm360@nyu.edu>
2024-03-07 10:59:29 -0500

    modified:   .gitignore 	modified:   .travis.yml 	modified:   taxiapp/taxiapp/settings.py

--- taxiapp/taxiapp/settings.py
+++ taxiapp/taxiapp/settings.py
@@ -20,12 +20,12 @@ COGNITO_AWS_REGION = env("COGNITO_AWS_REGION")
 """
 
 # #############Uncomment for travis deployment##############
-SECRET_KEY = os.environ["SECRET_KEY"]
-COGNITO_DOMAIN = os.environ["COGNITO_DOMAIN"]
-COGNITO_APP_CLIENT_SECRET = os.environ["COGNITO_APP_CLIENT_SECRET"]
-COGNITO_USER_POOL_ID = os.environ["COGNITO_USER_POOL_ID"]
-COGNITO_APP_CLIENT_ID = os.environ["COGNITO_APP_CLIENT_ID"]
-COGNITO_AWS_REGION = os.environ["COGNITO_AWS_REGION"]
+SECRET_KEY = os.environ.get("SECRET_KEY")
+COGNITO_DOMAIN = os.environ.get("COGNITO_DOMAIN")
+COGNITO_APP_CLIENT_SECRET = os.environ.get("COGNITO_APP_CLIENT_SECRET")
+COGNITO_USER_POOL_ID = os.environ.get("COGNITO_USER_POOL_ID")
+COGNITO_APP_CLIENT_ID = os.environ.get("COGNITO_APP_CLIENT_ID")
+COGNITO_AWS_REGION = os.environ.get("COGNITO_AWS_REGION")
 COGNITO_PUBLIC_KEYS_URL = f"https://cognito-idp.{COGNITO_AWS_REGION}.amazonaws.com/{COGNITO_USER_POOL_ID}/.well-known/jwks.json"
 
 # ##########################################################
