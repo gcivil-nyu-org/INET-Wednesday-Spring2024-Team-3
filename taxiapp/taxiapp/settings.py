8c319d3c69e60e92531542277b47d50ffcc37b73
devmcdonald <dpm360@nyu.edu>
2024-03-07 19:49:04 -0500

    modified:   .travis.yml 	modified:   taxiapp/.ebextensions/django.config 	modified:   taxiapp/db.sqlite3 	modified:   taxiapp/requirements.txt 	modified:   taxiapp/taxiapp/settings.py

--- taxiapp/taxiapp/settings.py
+++ taxiapp/taxiapp/settings.py
@@ -5,12 +5,28 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# env = environ.Env()
-# environ.Env.read_env(env_file="secrets.env")
+"""
+# ############Uncomment for local development, add secrets.env local file###############
+env = environ.Env()
+environ.Env.read_env(env_file="secrets.env")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: keep the secret key used in production secret!
+COGNITO_DOMAIN = env("COGNITO_DOMAIN")
+COGNITO_APP_CLIENT_SECRET = env("COGNITO_APP_CLIENT_SECRET")
+COGNITO_USER_POOL_ID = env("COGNITO_USER_POOL_ID")
+COGNITO_APP_CLIENT_ID = env("COGNITO_APP_CLIENT_ID")
+COGNITO_AWS_REGION = env("COGNITO_AWS_REGION")
+"""
+
+# #############Uncomment for travis deployment##############
 SECRET_KEY = os.environ.get("SECRET_KEY")
+COGNITO_DOMAIN = os.environ.get("COGNITO_DOMAIN")
+COGNITO_APP_CLIENT_SECRET = os.environ.get("COGNITO_APP_CLIENT_SECRET")
+COGNITO_USER_POOL_ID = os.environ.get("COGNITO_USER_POOL_ID")
+COGNITO_APP_CLIENT_ID = os.environ.get("COGNITO_APP_CLIENT_ID")
+COGNITO_AWS_REGION = os.environ.get("COGNITO_AWS_REGION")
+COGNITO_PUBLIC_KEYS_URL = f"https://cognito-idp.{COGNITO_AWS_REGION}.amazonaws.com/{COGNITO_USER_POOL_ID}/.well-known/jwks.json"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -25,14 +41,14 @@ AUTHENTICATION_BACKENDS = [
     "taxiapp.cognito_backend.CognitoBackend",
     "django.contrib.auth.backends.ModelBackend",
 ]
-
+"""
 COGNITO_DOMAIN = os.environ.get("COGNITO_DOMAIN")
 COGNITO_APP_CLIENT_SECRET = os.environ.get("COGNITO_APP_CLIENT_SECRET")
 COGNITO_USER_POOL_ID = os.environ.get("COGNITO_USER_POOL_ID")
 COGNITO_APP_CLIENT_ID = os.environ.get("COGNITO_APP_CLIENT_ID")
 COGNITO_AWS_REGION = os.environ.get("COGNITO_AWS_REGION")
 COGNITO_PUBLIC_KEYS_URL = f"https://cognito-idp.{COGNITO_AWS_REGION}.amazonaws.com/{COGNITO_USER_POOL_ID}/.well-known/jwks.json"
-
+"""
 
 INSTALLED_APPS = [
     "taxiapp",
