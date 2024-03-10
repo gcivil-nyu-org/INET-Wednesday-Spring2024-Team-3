cdc03761eab1c7749c107bbf4a8be8def6014825
Jeffer-Hernandez <58124434+Jeffer-Hernandez@users.noreply.github.com>
2024-03-09 20:27:19 -0500

    Format with Black

--- taxiapp/rideshare/urls.py
+++ taxiapp/rideshare/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.rideshare_home_view, name='rideshare_home_view'),
+    path("", views.rideshare_home_view, name="rideshare_home_view"),
 ]
--- taxiapp/rideshare/views.py
+++ taxiapp/rideshare/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def rideshare_home_view(request):
     return HttpResponse("Hello, this is the rideshare home view!")
