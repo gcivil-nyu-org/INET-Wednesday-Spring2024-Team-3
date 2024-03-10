932b5704113ab6fd7227e4beef50e5683ac9860d
Jeffer-Hernandez <58124434+Jeffer-Hernandez@users.noreply.github.com>
2024-03-10 14:01:33 -0400

    Finished basic Forum app functionality, post, comment etc.

--- taxiapp/forum/urls.py
+++ taxiapp/forum/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.forum_home_view, name="forum_home_view"),
+    path("", views.forum_home, name="forum_home"),
+    path('post/create/', views.post_create, name='post_create'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('post/<int:post_id>/add_comment/', views.add_comment, name='add_comment'),
 ]
