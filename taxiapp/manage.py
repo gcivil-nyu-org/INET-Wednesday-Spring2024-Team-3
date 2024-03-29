f76eafe7222fff7e87c483bcc40eaafcb03e08c0
devmcdonald <dpm360@nyu.edu>
2024-03-29 17:30:11 -0400

    modified:   .travis.yml 	modified:   taxiapp/.ebextensions/django.config 	modified:   taxiapp/manage.py 	modified:   taxiapp/taxiapp/wsgi.py

--- taxiapp/manage.py
+++ taxiapp/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taxiapp.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taxiapp.taxiapp.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
